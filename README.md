fef6538019e5162199fefd27e243ad73e3cec41f
Adryel Santos <adryeljunio2020@gmail.com>
2023-07-21 12:27:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hello_world
+ Projeto em flutter
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Meu aplicativo',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+        print('Click');
+      }),
+    );
+  }
+}
